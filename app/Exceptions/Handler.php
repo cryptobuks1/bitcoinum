3381121dcc9c675243d0f4602eb1d6e7e8f575c7
hotsaucejake <get@hotsaucejake.com>
2018-08-31 11:35:36 -0400

    users can publish a new thread
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,7 +47,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if (app()->environment === 'testing') throw $exception;
+        //if (app()->environment === 'testing') throw $exception;
         
         return parent::render($request, $exception);
     }
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -21,6 +21,15 @@ class CreateThreadsTest extends TestCase
         $this->post('threads', $thread->toArray());
     }
 
+
+    /** @test */
+    function guests_cannot_see_the_create_threads_page()
+    {
+        $this->get('/threads/create')
+                ->assertRedirect('login');
+    }
+
+
     /** @test */
     function an_authenticated_user_can_create_new_forum_threads()
     {
