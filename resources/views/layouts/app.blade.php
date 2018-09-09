1cad0d9421d0965699a54c1116366d42d0214fbf
hotsaucejake <get@hotsaucejake.com>
2018-09-09 17:34:41 -0400

    a user can filter thereads by popularity
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,7 @@
                                 @if(auth()->check())
                                     <a class="dropdown-item" href="/threads?by={{ auth()->user()->name }}">My Threads</a>
                                 @endif
+                                <a class="dropdown-item" href="/threads?popular=1">Popular Threads</a>
                             </div>
                         </li>
                         <li class="nav-item"><a class="nav-link" href="/threads/create">New Thread</a></li>
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -9,16 +9,24 @@
 
                 <div class="card-body">
                     @foreach($threads as $thread)
-                        <article>
-                            <h4>
-                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                            </h4>
-                            <div class="body">
-                                {{ $thread->body }}
+                        <div class="card mb-3">
+                            <div class="card-header p-2">
+                                <h4 class="m-0"><a href="{{ $thread->path() }}">{{ $thread->title }}</a></h4>
                             </div>
-                        </article>
-
-                        <hr>
+                            <div class="card-body">
+                                    {{ $thread->body }}
+                            </div>
+                            <div class="card-footer d-flex justify-content-between p-0">
+                                <div class="p-2">
+                                    <a href="#">{{ $thread->creator->name }}</a>: {{ $thread->created_at->diffForHumans() }}
+                                </div>
+                                <div class="p-2">
+                                    <a href="{{ $thread->path() }}">
+                                        {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                 </div>
             </div>
