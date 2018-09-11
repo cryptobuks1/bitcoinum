82438d36d771fcfca93d3f6a5c5757595f04058f
hotsaucejake <get@hotsaucejake.com>
2018-09-11 15:56:51 -0400

    create Favoritable trait
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Reply.php
+++ app/Reply.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Favorite;
+use App\Traits\Favoritable;
 
 class Reply extends Model
 {
+    use Favoritable;
+
+
     protected $guarded = [];
 
     protected $with = ['owner', 'favorites'];
@@ -15,34 +18,5 @@ class Reply extends Model
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
-    }
-
-
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-
-        if(! $this->favorites()->where($attributes)->exists())
-        {
-            return $this->favorites()->create($attributes);
-        }
-    }
-
-
-    public function isFavorited()
-    {
-        return !! $this->favorites->where('user_id', auth()->id())->count();
-    }
-
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
     }
 }
