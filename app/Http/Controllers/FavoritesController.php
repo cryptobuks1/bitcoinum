9d4e94dd31a3875163204e4623a77decfb3f9f1a
hotsaucejake <get@hotsaucejake.com>
2018-09-11 13:46:42 -0400

    show favorite button for replies
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -42,7 +42,9 @@ class FavoritesController extends Controller
      */
     public function store(Reply $reply)
     {
-        return $reply->favorite();
+        $reply->favorite();
+
+        return back();
     }
 
     /**
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,9 +1,19 @@
 <div class="card mb-3">
-    <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> 
-        said {{ $reply->created_at->diffForHumans() }}...
+    <div class="card-header d-flex justify-content-between p-0">
+        <div class="p-2">
+            <a href="#">
+                {{ $reply->owner->name }}
+            </a> 
+            said {{ $reply->created_at->diffForHumans() }}...
+        </div>
+        <div class="p-2">
+            <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-outline-primary btn-sm" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                    {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                </button>
+            </form>
+        </div>
     </div>
     
     <div class="card-body">
